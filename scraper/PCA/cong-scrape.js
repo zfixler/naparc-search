30b8cf248219d01a6173bc024ed61408ba15695b
Zach Fixler <zakfixler@gmail.com>
2021-03-06 15:32:56 -0500

    pca scrape almost completed

--- scraper/PCA/cong-scrape.js
+++ scraper/PCA/cong-scrape.js
@@ -1,22 +1,126 @@
-const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { v4: uuidv4 } = require('uuid');
 
-async function load(){
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-        })
-    const page = await browser.newPage()
-    await page.setDefaultNavigationTimeout(30000); 
+const pca = [];
+let count = 0;
 
-    await page.goto('https://stat.pcanet.org/ac/directory/directory.cfm')
-    
+async function getLongLat(city, state) {
+	const page = await fetch(`https://stat.pcanet.org/ac/directory/${href}`);
+	const html = await page.text();
+	const $ = cheerio.load(html);
 
-    // const data = await page.evaluate(() => {
-    //     document.querySelector('body').textContent
-    // })
-
-    console.log(await page.content())
-
-    await browser.close()
+	return $('textarea').text();
 }
 
-load().catch(error => console.log(error))
+async function scrapePage(html) {
+	const $ = cheerio.load(html);
+	const congArr = [];
+
+	$('.formtableleft').each((i, el) => {
+		const name = $(el).children().text();
+		const phone = $(el).siblings().first().next().next().next().next().text();
+		const website = $(el)
+			.siblings()
+			.first()
+			.next()
+			.next()
+			.next()
+			.next()
+			.next()
+			.next()
+			.children()
+			.text();
+		const email = $(el)
+			.siblings()
+			.first()
+			.next()
+			.next()
+			.next()
+			.next()
+			.next()
+			.children()
+			.text();
+		const pastor = $(el).siblings().last().text();
+
+		const city = $(el).next().text().toLowerCase();
+		const state = $(el).next().next().text().toLowerCase();
+
+		const date = new Date();
+		const update = `Updated on ${
+			date.getMonth() + 1
+		}/${date.getDate()}/${date.getFullYear()}.`;
+
+		const cong = {
+			id: uuidv4(),
+			name: name,
+			phone: phone,
+			pastor: pastor,
+			email: email,
+			website: website,
+			address: `${city}, ${state}`,
+			city: city,
+			state: state,
+			date: update,
+		};
+
+		congArr.push(cong);
+	});
+
+	return congArr;
+}
+
+async function getStateOptions() {
+	const page = await fetch(
+		'https://stat.pcanet.org/ac/directory/directory.cfm'
+	);
+	const html = await page.text();
+
+	const $ = cheerio.load(html);
+
+	const stateSelectorArray = [];
+
+	$('#State')
+		.children()
+		.each((i, el) => {
+			stateSelectorArray.push($(el).attr('value'));
+		});
+
+	return stateSelectorArray;
+}
+
+async function fetchPage(url, data) {
+	const page = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: data,
+	});
+
+	return await page.text();
+}
+
+async function getPages() {
+	const states = await getStateOptions().catch((error) => console.log(error));
+	for await (state of states) {
+		if (state !== 'Select State') {
+			const data = `State=${state}&orderby=1`;
+			const url = 'https://stat.pcanet.org/ac/directory/directory.cfm';
+			const html = await fetchPage(url, data);
+			const stateCongs = await scrapePage(html).catch((error) =>
+				console.log(error)
+			);
+            count += stateCongs.length
+			pca.push(stateCongs);
+            console.log(`Scraped ${pca.length} states and ${count} congregations.`)
+		}
+	}
+
+	const data = JSON.stringify(pca)
+    fs.writeFileSync('../../frontend/src/api/pca.json', data);
+	console.log('Created json');
+}
+
+getPages().catch((error) => console.log(error));
---
+++ scraper/PCA/test.js
@@ -0,0 +1,18 @@
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+let href = 'churchinfo.cfm?OrgID=1152'
+
+async function getAddress(href) {
+	const page = 
+        await fetch(`https://stat.pcanet.org/ac/directory/${href}`);
+
+	const html = await page.text();
+	const $ = cheerio.load(html);
+
+	return $('textarea').attr('value')
+}
+
+getAddress(href)
+    .then(res => console.log(res))
+    .catch(error => console.log(error))
