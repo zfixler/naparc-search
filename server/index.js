2bca814f91c082c561be626c66cc743e5d2c1c6d
Zach Fixler <zakfixler@gmail.com>
2021-03-22 06:26:44 -0400

    cron job edit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
 	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-cron.schedule('15 6 * * MON,TUE', () => {
+cron.schedule('30 6 * * 1,4', () => {
 	require(path.join(__dirname, 'arp-scrape.js'));
 	require(path.join(__dirname, 'rpcna-scrape.js'));
 	require(path.join(__dirname, 'opc-scrape.js'));
