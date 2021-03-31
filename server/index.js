1792564430b3e2d4fa6184efe108b22af7b7b256
Zach Fixler <zakfixler@gmail.com>
2021-03-31 06:19:26 -0400

    test

--- server/index.js
+++ server/index.js
@@ -15,44 +15,44 @@ cron.schedule('16 6 * * *', () => {
 })
 
 
-cron.schedule('45 5 * * *', () => {
-	shell.cd(path.join(__dirname)).exec('node urcna-scrape.js')
-	console.log('cron fired')
+cron.schedule('25 6 * * *', () => {
+	shell.exec('node urcna-scrape.js')
+	console.log('cron fired at 6:25 am')
   });
 
-cron.schedule('46 5 * * *', () => {
-	shell.cd(path.join(__dirname)).exec('node arp-scrape.js')
-	console.log('cron fired')
+cron.schedule('26 6 * * *', () => {
+	shell.exec('node arp-scrape.js')
+	console.log('cron fired at 6:26 am')
   });
 
-cron.schedule('47 5 * * *', () => {
+cron.schedule('27 6 * * *', () => {
 	shell.exec('node opc-scrape.js')
-	console.log('cron fired')
+	console.log('cron fired at 6:27 am')
   });
 
-  cron.schedule('48 5 * * *', () => {
+  cron.schedule('28 6 * * *', () => {
 	shell.exec('node pca-scrape.js')
-	console.log('cron fired')
+	console.log('cron fired at 6:28 am')
   });
 
-  cron.schedule('49 5 * * *', () => {
-	shell.cd(path.join(__dirname)).exec('node prc-scrape.js')
-	console.log('cron fired')
+  cron.schedule('30 6 * * *', () => {
+	shell.exec('node prc-scrape.js')
+	console.log('cron fired at 6:30 am')
   });
 
-  cron.schedule('50 5 * * *', () => {
+  cron.schedule('31 6 * * *', () => {
 	shell.exec('node hrc-scrape.js')
-	console.log('cron fired')
+	console.log('cron fired at 6:31 am')
   });
 
-  cron.schedule('51 5 * * *', () => {
+  cron.schedule('32 6 * * *', () => {
 	shell.exec('node rpcna-scrape.js')
-	console.log('cron fired')
+	console.log('cron fired at 6:32 am')
   });
 
-cron.schedule('55 5 * * *', () => {
+cron.schedule('40 6 * * *', () => {
 	shell.cd(path.join(__dirname, '..')).exec('npm build')
-	console.log('cron fired')
+	console.log('cron fired at 6:40 am. task complete.')
   });
 
 const PORT = 13373;
