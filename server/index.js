562cc04804e68bdf57f7af5a34a112625a23f7d4
Zach Fixler <zakfixler@gmail.com>
2021-03-31 06:38:17 -0400

    test

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const cron = require('node-cron');
 const shell = require('shelljs');
+const { dir } = require('node:console');
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
@@ -10,49 +11,45 @@ app.use((req, res, next) => {
 	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-cron.schedule('16 6 * * *', () => {
-	console.log('cron fired at 6:16am')
-})
 
-
-cron.schedule('25 6 * * *', () => {
-	shell.exec('node urcna-scrape.js')
-	console.log('cron fired at 6:25 am')
+cron.schedule('41 6 * * *', () => {
+	shell.cd(dir).exec('node urcna-scrape.js')
+	console.log('cron fired at 6:41 am')
   });
 
-cron.schedule('26 6 * * *', () => {
-	shell.exec('node arp-scrape.js')
-	console.log('cron fired at 6:26 am')
+cron.schedule('42 6 * * *', () => {
+	shell.cd(dir).exec('node arp-scrape.js')
+	console.log('cron fired at 6:42 am')
   });
 
-cron.schedule('27 6 * * *', () => {
-	shell.exec('node opc-scrape.js')
-	console.log('cron fired at 6:27 am')
+cron.schedule('43 6 * * *', () => {
+	shell.cd(dir).exec('node opc-scrape.js')
+	console.log('cron fired at 6:43 am')
   });
 
-  cron.schedule('28 6 * * *', () => {
-	shell.exec('node pca-scrape.js')
-	console.log('cron fired at 6:28 am')
+  cron.schedule('44 6 * * *', () => {
+	shell.cd(dir).exec('node pca-scrape.js')
+	console.log('cron fired at 6:44 am')
   });
 
-  cron.schedule('30 6 * * *', () => {
-	shell.exec('node prc-scrape.js')
-	console.log('cron fired at 6:30 am')
+  cron.schedule('45 6 * * *', () => {
+	shell.cd(dir).exec('node prc-scrape.js')
+	console.log('cron fired at 6:45 am')
   });
 
-  cron.schedule('31 6 * * *', () => {
-	shell.exec('node hrc-scrape.js')
-	console.log('cron fired at 6:31 am')
+  cron.schedule('46 6 * * *', () => {
+	shell.cd(dir).exec('node hrc-scrape.js')
+	console.log('cron fired at 6:46 am')
   });
 
-  cron.schedule('32 6 * * *', () => {
-	shell.exec('node rpcna-scrape.js')
-	console.log('cron fired at 6:32 am')
+  cron.schedule('47 6 * * *', () => {
+	shell.cd(dir).exec('node rpcna-scrape.js')
+	console.log('cron fired at 6:47 am')
   });
 
-cron.schedule('40 6 * * *', () => {
+cron.schedule('53 6 * * *', () => {
 	shell.cd(path.join(__dirname, '..')).exec('npm build')
-	console.log('cron fired at 6:40 am. task complete.')
+	console.log('cron fired at 6:53 am. task complete.')
   });
 
 const PORT = 13373;
