8b785572eb2adcd411d04dfc3cf6165c0d794bc3
Zach Fixler <zakfixler@gmail.com>
2021-03-30 21:17:35 -0400

    cron timing

--- server/index.js
+++ server/index.js
@@ -11,35 +11,35 @@ app.use((req, res, next) => {
 });
 
 
-cron.schedule('0 21 * * *', () => {
+cron.schedule('20 21 * * *', () => {
 	shell.exec('node urcna-scrape.js')
   });
 
-cron.schedule('2 21 * * *', () => {
+cron.schedule('21 21 * * *', () => {
 	shell.exec('node arp-scrape.js')
   });
 
-cron.schedule('3 21 * * *', () => {
+cron.schedule('22 21 * * *', () => {
 	shell.exec('node opc-scrape.js')
   });
 
-  cron.schedule('5 21 * * *', () => {
+  cron.schedule('25 21 * * *', () => {
 	shell.exec('node pca-scrape.js')
   });
 
-  cron.schedule('8 21 * * *', () => {
+  cron.schedule('28 21 * * *', () => {
 	shell.exec('node prc-scrape.js')
   });
 
-  cron.schedule('11 21 * * *', () => {
+  cron.schedule('29 21 * * *', () => {
 	shell.exec('node hrc-scrape.js')
   });
 
-  cron.schedule('13 21 * * *', () => {
+  cron.schedule('30 21 * * *', () => {
 	shell.exec('node rpcna-scrape.js')
   });
 
-cron.schedule('20 21 * * *', () => {
+cron.schedule('35 21 * * *', () => {
 	shell.cd(path.join(__dirname, '..')).exec('npm build')
   });
 
