d041ddea8ab29c2130a67bcf50eccb70ab3280d5
Zach Fixler <zakfixler@gmail.com>
2021-03-30 20:09:34 -0400

    test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cron = require('node-cron');
-const fetchUrl = require('./urcna-scrape.js')
+// const { createArpJson } = require(path.join(__dirname, 'arp-scrape.js'));
+// const { getURL } = require(path.join(__dirname, 'rpcna-scrape.js'));
+// const { scrapeData } = require(path.join(__dirname, 'opc-scrape.js'));
+// const { getPages } = require(path.join(__dirname, 'pca-scrape.js'));
+const { fetchUrl } = require(path.join(__dirname, 'urcna-scrape.js'));
+// const { getUrls }= require(path.join(__dirname, 'prc-scrape.js'));
+// const { getLongLat } = require(path.join(__dirname, 'hrc-scrape.js'));
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
@@ -10,7 +16,7 @@ app.use((req, res, next) => {
 	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-cron.schedule('*/2 * * * *', () => {
+cron.schedule('*/3 * * * *', () => {
 	console.log('scrape cron running')
 	fetchUrl.catch(error => console.log(error))
   });
