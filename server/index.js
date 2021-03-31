5431da184d7298f7024b9bed105ab646d5d800e4
Zach Fixler <zakfixler@gmail.com>
2021-03-30 21:42:41 -0400

    test

--- server/index.js
+++ server/index.js
@@ -11,35 +11,35 @@ app.use((req, res, next) => {
 });
 
 
-cron.schedule('20 21 * * *', () => {
-	shell.exec('node urcna-scrape.js')
+cron.schedule('45 21 * * *', () => {
+	shell.cd(path.join(__dirname)).exec('node urcna-scrape.js')
   });
 
-cron.schedule('21 21 * * *', () => {
-	shell.exec('node arp-scrape.js')
+cron.schedule('46 21 * * *', () => {
+	shell.cd(path.join(__dirname)).exec('node arp-scrape.js')
   });
 
-cron.schedule('22 21 * * *', () => {
-	shell.exec('node opc-scrape.js')
+cron.schedule('47 21 * * *', () => {
+	shell.cd(path.join(__dirname)).exec('node opc-scrape.js')
   });
 
-  cron.schedule('25 21 * * *', () => {
-	shell.exec('node pca-scrape.js')
+  cron.schedule('48 21 * * *', () => {
+	shell.cd(path.join(__dirname)).exec('node pca-scrape.js')
   });
 
-  cron.schedule('28 21 * * *', () => {
-	shell.exec('node prc-scrape.js')
+  cron.schedule('49 21 * * *', () => {
+	shell.cd(path.join(__dirname)).cd(path.join(__dirname)).exec('node prc-scrape.js')
   });
 
-  cron.schedule('29 21 * * *', () => {
-	shell.exec('node hrc-scrape.js')
+  cron.schedule('50 21 * * *', () => {
+	shell.cd(path.join(__dirname)).exec('node hrc-scrape.js')
   });
 
-  cron.schedule('30 21 * * *', () => {
-	shell.exec('node rpcna-scrape.js')
+  cron.schedule('51 21 * * *', () => {
+	shell.cd(path.join(__dirname)).exec('node rpcna-scrape.js')
   });
 
-cron.schedule('35 21 * * *', () => {
+cron.schedule('55 21 * * *', () => {
 	shell.cd(path.join(__dirname, '..')).exec('npm build')
   });
 
